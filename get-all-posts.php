5c2e7b8576661657cc4456b8ea1da674e09712cc
Amit S Namboothiry <me@amitsn.com>
2015-09-19 11:54:42 +0530

    Pagination and posts per page

--- get-all-posts.php
+++ get-all-posts.php
@@ -1,25 +1,64 @@
 <?php
+    $paged = ($wp->query_vars['page'])?$wp->query_vars['page']:1;
+    $posts_per_page = (get_option('lightwprest_posts_per_page') != '')?intval(get_option('lightwprest_posts_per_page')):10;
 	$post_query = new WP_Query();
-	$posts_list = $post_query->query();
+	$posts_list = $post_query->query(
+            array(
+                'paged'            =>  $paged,
+                'posts_per_page'   =>  $posts_per_page
+            )
+		);
 	$response = array();
+
+	//Fetch Plugin Options
+    $lightwprest_posts_fields = (get_option('lightwprest_posts_fields') != '')?get_option('lightwprest_posts_fields'):'[]';
+	$show_id = in_array("id", $lightwprest_posts_fields)?true:false;
+	$show_title = in_array("title", $lightwprest_posts_fields)?true:false;
+	$show_status = in_array("status", $lightwprest_posts_fields)?true:false;
+	$show_type = in_array("type", $lightwprest_posts_fields)?true:false;
+	$show_author = in_array("author", $lightwprest_posts_fields)?true:false;
+	$show_content = in_array("content", $lightwprest_posts_fields)?true:false;
+	$show_featured_image = in_array("featuredimg", $lightwprest_posts_fields)?true:false;
+	$show_parent = in_array("parent", $lightwprest_posts_fields)?true:false;
+	$show_link = in_array("link", $lightwprest_posts_fields)?true:false;
 
 	foreach ($posts_list as $post) {
 		$post = get_object_vars($post);
-		$post_obj = get_post( $post['ID'] );
+		$post_obj = get_post($post['ID']);
 
 		$GLOBALS['post'] = $post_obj;
-		setup_postdata( $post_obj );
-
+		setup_postdata($post_obj);
+        
+        $post_fields = array();
+		
 		// prepare common post fields
-		$post_fields = array(
-			'title'           => get_the_title( $post['ID'] ), // $post['post_title'],
-			'status'          => $post['post_status'],
-			'type'            => $post['post_type'],
-			'author'          => (int) $post['post_author'],
-			'content'         => apply_filters( 'the_content', $post['post_content'] ),
-			'parent'          => (int) $post['post_parent'],
-			'link'            => get_permalink( $post['ID'] ),
-		);
+		if($show_id) {
+			$post_fields['id'] = $post['ID'];
+		}
+		if($show_title) {
+			$post_fields['title'] = get_the_title($post['ID']);
+		}
+		if($show_status) {
+			$post_fields['status'] = $post['post_status'];
+		}
+		if($show_type) {
+			$post_fields['type'] = $post['post_type'];
+		}
+		if($show_author) {
+			$post_fields['author'] = (int) $post['post_author'];
+		}
+		if($show_content) {
+			$post_fields['content'] = apply_filters('the_content', $post['post_content']);
+		}
+		if($show_featured_image) {
+			$post_fields['featured_image'] = wp_get_attachment_url(get_post_thumbnail_id($post->ID, 'thumbnail'));
+		}
+		if($show_parent) {
+			$post_fields['parent'] = (int) $post['post_parent'];
+		}
+		if($show_link) {
+			$post_fields['link'] = get_permalink($post['ID']);
+		}
 
 		array_push($response, $post_fields);
 	}
--- light-wp-rest.php
+++ light-wp-rest.php
@@ -53,6 +53,7 @@ function wordpress_rest_plugin_settings() {
 
 function wordpress_rest_display_settings() {
     $lightwprest_posts_fields = (get_option('lightwprest_posts_fields') != '') ? get_option('lightwprest_posts_fields') : '[]';
+    $lightwprest_posts_per_page = (get_option('lightwprest_posts_per_page') != '') ? get_option('lightwprest_posts_per_page') : '10';
     $html = '
         </pre>
             <div class="wrap"><form action="options.php" method="post" name="options">
@@ -65,17 +66,29 @@ function wordpress_rest_display_settings() {
                     <tbody>
                         <tr>
                             <th scope="row">
-                                <label>Fields to show in /restify/posts</label>
+                                <label>Fields in All Posts JSON</label>
                             </th>
 
                             <td>
+                                <input type="checkbox" name="lightwprest_posts_fields[]" value="id" '.(in_array("id", $lightwprest_posts_fields)?'checked="checked"':'').'/>ID<br />
                                 <input type="checkbox" name="lightwprest_posts_fields[]" value="title" '.(in_array("title", $lightwprest_posts_fields)?'checked="checked"':'').'/>Title<br />
                                 <input type="checkbox" name="lightwprest_posts_fields[]" value="status" '.(in_array("status", $lightwprest_posts_fields)?'checked="checked"':'').'/>Status<br />
                                 <input type="checkbox" name="lightwprest_posts_fields[]" value="type" '.(in_array("type", $lightwprest_posts_fields)?'checked="checked"':'').'/>Type<br />
                                 <input type="checkbox" name="lightwprest_posts_fields[]" value="author" '.(in_array("author", $lightwprest_posts_fields)?'checked="checked"':'').'/>Author<br />
                                 <input type="checkbox" name="lightwprest_posts_fields[]" value="content" '.(in_array("content", $lightwprest_posts_fields)?'checked="checked"':'').'/>Content<br />
+                                <input type="checkbox" name="lightwprest_posts_fields[]" value="featuredimg" '.(in_array("featuredimg", $lightwprest_posts_fields)?'checked="checked"':'').'/>Featured Image<br />
                                 <input type="checkbox" name="lightwprest_posts_fields[]" value="parent" '.(in_array("parent", $lightwprest_posts_fields)?'checked="checked"':'').'/>Parent<br />
                                 <input type="checkbox" name="lightwprest_posts_fields[]" value="link" '.(in_array("link", $lightwprest_posts_fields)?'checked="checked"':'').'/>Link<br />
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <th scope="row">
+                                <label>Posts per Page</label>
+                            </th>
+
+                            <td>
+                                <input type="number" name="lightwprest_posts_per_page" value="'.$lightwprest_posts_per_page.'"/>
                             </td>
                         </tr>
                     </tbody>
@@ -85,7 +98,7 @@ function wordpress_rest_display_settings() {
  
                 <input type="hidden" name="action" value="update" />
 
-                <input type="hidden" name="page_options" value="lightwprest_posts_fields" />
+                <input type="hidden" name="page_options" value="lightwprest_posts_fields,lightwprest_posts_per_page" />
 
                 <input type="submit" name="Submit" value="Update" class="button-primary" id="submitbutton" /></form></div>
         <pre>
